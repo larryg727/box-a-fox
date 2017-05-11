9d447f41d2dd38013350b73a63f7261263715c8c
larryg727 <larrygonzales727@gmail.com>
2017-05-10 23:03:53 -0500

    -points now only score if fox is animated and clicked

--- js/fox.js
+++ js/fox.js
@@ -7,9 +7,12 @@ $(document).ready(function () {
     /*-------global variables----------*/
     var random;
     var idVar;
-    var timeLeft = 5;
+    var timeLeft = 30;//timer start value
     var score;
-    var timing;
+    var timing; // timer interval variable
+    var speed;//default fox speed
+    var gameSpeed;
+    var continuePlay;
 
     /*-------functions-----------*/
 
@@ -28,22 +31,46 @@ $(document).ready(function () {
     // function to animate random fox
     function playGame(){
         idVar = "#h-" + random;
+        $(idVar).children().attr("src", "img/fox2.png");
+        $(idVar).parent().attr("clickable", "true");
         $(idVar).children().animate({
             bottom: "-40px"
         }, 1500).animate({
             bottom: "-140px"
-        }, 1500, function(){
-            $(idVar).children().attr("src", "img/fox2.png");
-        });
+        }, 1500);
+        setTimeout(function(){
+            $(".hide").attr("clickable", "false");
+        }, 2600);
     }
+
+
 
     //keeps game going at interval
     function gameFlow(){
-        setInterval(function(){
+        if(continuePlay) {   //stops function recursion
+        speed = 3000; // sets initial speed.
+        if(score > 20){   // speed increases in foxes
+            speed = 500;
+        }else if(score >12){
+            speed = 1000;
+        }else if(score > 8){
+            speed = 1500;
+        }else if(score > 4){
+            speed = 2000;
+        }else if(score > 1){
+            speed = 2400;
+        }
+        gameSpeed = setInterval(function(){
             randomGen();
             playGame();
-        }, 3000)
 
+    }, speed);
+
+                setTimeout(function () {  // time out allows snimation to be played before evluation
+                    clearInterval(gameSpeed); //allows speed change to take effect
+                    gameFlow();
+                }, speed);
+            }
     }
 
     //timer function
@@ -64,6 +91,7 @@ $(document).ready(function () {
             $("#time").slideDown(1000);
         });
         score = 0;
+        continuePlay = true;
         gameFlow();
         setTimeout(function(){
             timing = setInterval(timer, 1500);
@@ -73,13 +101,21 @@ $(document).ready(function () {
     //click functions
     $(".hide").click(function(){
         $(this).children().children().attr("src", "img/foxhit.png");
-        score += 1;
-        $("#score").text(score);
-    })
+       var clickable = $(this).attr("clickable");
+       console.log(clickable);
+        if(clickable === "true") {
+            score += 1;
+            $("#score").text(score);
+        }
+
+    });
+
 
     //gameover function
     function gameOver(){
+        continuePlay = false;
         clearInterval(timing);
+        clearInterval(gameSpeed);
         $("#gameOver").show();
     }
 
